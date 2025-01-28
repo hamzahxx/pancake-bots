4db32c722e9136467e4a2743a5a217c284eb744f
Hamzah <hamzahchhapra2004@gmail.com>
2025-01-29 03:35:20 +0530

    fix/background
    - fixed background by using top postioning above lg screen instead of bottom positioning below lg screen
    - removed unnecessary text and css classes of those text

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,13 @@ import background from "./assets/background.svg";
 function App() {
   return (
     <div className="bg-[#0B1120] flex h-screen items-center justify-center">
-      <div className="text-5xl relative z-10 text-white font-extrabold">
-        Content
+      <div className="text-5xl relative z-10">
+        
       </div>
-      <img className="min-w-screen absolute bottom-0 z-[1]" src={background} />
+      <img
+        className="min-w-screen absolute bottom-0 lg:top-[-10px] z-[1]"
+        src={background}
+      />
     </div>
   );
 }
