b984ef2e649e051b634a9af338e9986fab7d8872
Hamzah <hamzahchhapra2004@gmail.com>
2025-01-30 13:00:48 +0530

    animate/Authentication.jsx
    - fade in for initial load
    - slide out and slide in for transitions between active forms

--- src/components/Authentication.jsx
+++ src/components/Authentication.jsx
@@ -1,20 +1,37 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import googleIcon from "../assets/google-icon.svg";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 const AuthComponent = () => {
-  const [showLoginForm, setShowLoginForm] = useState(false);
-  const [showSignupForm, setShowSignupForm] = useState(false);
+  const [activeForm, setActiveForm] = useState("initial");
   const [showPassword, setShowPassword] = useState(false);
+  const containerRef = useRef(null);
+  const loginRef = useRef(null);
+  const signupRef = useRef(null);
+  const ANIMATION_DURATION = 0.3;
 
-  const handleLoginClick = () => {
-    setShowLoginForm(true);
-    setShowSignupForm(false);
-  };
-
-  const handleSignupClick = () => {
-    setShowSignupForm(true);
-    setShowLoginForm(false);
-  };
+  function handleFormChange(form) {
+    gsap.to(containerRef.current, {
+      opacity: 0,
+      y: -10,
+      ease: "power1.inOut",
+      duration: ANIMATION_DURATION,
+      onComplete: () => {
+        setActiveForm(form);
+        gsap.fromTo(
+          containerRef.current,
+          { opacity: 0, y: 10 },
+          {
+            opacity: 1,
+            y: 0,
+            ease: "power1.inOut",
+            duration: ANIMATION_DURATION,
+          }
+        );
+      },
+    });
+  }
 
   const handleContinueWithGoogle = () => {
     // Handle Google authentication logic here
@@ -27,27 +44,40 @@ const AuthComponent = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     // Handle form submission logic here
-    if (showLoginForm) {
+    if (activeForm == "login") {
       console.log("Login form submitted");
-    } else if (showSignupForm) {
+      handleFormChange("initial");
+    } else if (activeForm == "signup") {
       console.log("Signup form submitted");
+      handleFormChange("initial");
     }
   };
 
+  useGSAP(
+    () => {
+      const tl = gsap.timeline();
+      tl.from(containerRef.current, {
+        delay: 0.5,
+        opacity: 0,
+        duration: ANIMATION_DURATION,
+      });
+    },
+    { scope: containerRef }
+  );
   return (
-    <div className="auth-container">
-      {!showLoginForm && !showSignupForm && (
+    <div className="auth-container" ref={containerRef}>
+      {activeForm === "initial" && (
         <div className="flex flex-col gap-y-4">
           <div className="flex flex-col md:flex-row justify-center gap-4">
             <button
-              className="w-56 md:w-26 h-10 bg-blue-500 text-white rounded-lg"
-              onClick={handleLoginClick}
+              className="w-56 md:w-26 h-10 cursor-pointer bg-blue-500 text-white rounded-lg"
+              onClick={() => handleFormChange("login")}
             >
               Login
             </button>
             <button
-              className="w-56 md:w-26 h-10 bg-blue-500 text-white rounded-lg"
-              onClick={handleSignupClick}
+              className="w-56 md:w-26 h-10 cursor-pointer bg-blue-500 text-white rounded-lg"
+              onClick={() => handleFormChange("signup")}
             >
               Sign Up
             </button>
@@ -63,12 +93,23 @@ const AuthComponent = () => {
         </div>
       )}
 
-      {showLoginForm && (
-        <div className="flex">
+      {(activeForm === "login" || activeForm === "signup") && (
+        <div
+          className="flex"
+          ref={activeForm === "login" ? loginRef : signupRef}
+        >
           <form
             className="flex flex-col gap-y-4 justify-between items-center"
             onSubmit={handleSubmit}
           >
+            {activeForm === "signup" && (
+              <input
+                className="bg-white text-black w-56 h-10 px-5 rounded-lg duration-150 focus:invalid:text-pink-600 focus:border-sky-500 focus:outline focus:outline-sky-500 focus:invalid:border-pink-500 focus:invalid:outline-pink-500 disabled:border-gray-200 disabled:bg-gray-50 disabled:text-gray-500 disabled:shadow-none dark:disabled:border-gray-700 dark:disabled:bg-gray-800/20"
+                type="name"
+                placeholder="Name"
+                required
+              />
+            )}
             <input
               className="bg-white text-black w-56 h-10 px-5 rounded-lg duration-150 focus:invalid:text-pink-600 focus:border-sky-500 focus:outline focus:outline-sky-500 focus:invalid:border-pink-500 focus:invalid:outline-pink-500 disabled:border-gray-200 disabled:bg-gray-50 disabled:text-gray-500 disabled:shadow-none dark:disabled:border-gray-700 dark:disabled:bg-gray-800/20"
               type="email"
@@ -80,61 +121,19 @@ const AuthComponent = () => {
                 className="bg-white text-black w-56 h-10 px-5 pr-15 rounded-lg duration-150 focus:invalid:text-pink-600 focus:border-sky-500 focus:outline focus:outline-sky-500 focus:invalid:border-pink-500 focus:invalid:outline-pink-500 disabled:border-gray-200 disabled:bg-gray-50 disabled:text-gray-500 disabled:shadow-none dark:disabled:border-gray-700 dark:disabled:bg-gray-800/20"
                 type={showPassword ? "text" : "password"}
                 placeholder="Password"
+                minLength={8}
                 required
               />
               <button
                 type="button"
-                className="absolute my-1 right-1 bg-gray-200 hover:bg-gray-300 rounded px-2 py-1"
+                className="absolute my-1 right-1 cursor-pointer bg-gray-200 hover:bg-gray-300 rounded px-2 py-1"
                 onClick={togglePasswordVisibility}
               >
                 {showPassword ? "Hide" : "Show"}
               </button>
             </div>
             <button
-              className="bg-green-400 hover:bg-green-300 duration-150 text-black hover:font-bold w-56 h-10 rounded-lg flex justify-center items-center"
-              type="submit"
-            >
-              Submit
-            </button>
-          </form>
-        </div>
-      )}
-
-      {showSignupForm && (
-        <div className="flex">
-          <form
-            className="flex flex-col gap-y-4 justify-between items-center"
-            onSubmit={handleSubmit}
-          >
-            <input
-              className="bg-white text-black w-56 h-10 px-5 rounded-lg duration-150 focus:invalid:text-pink-600 focus:border-sky-500 focus:outline focus:outline-sky-500 focus:invalid:border-pink-500 focus:invalid:outline-pink-500 disabled:border-gray-200 disabled:bg-gray-50 disabled:text-gray-500 disabled:shadow-none dark:disabled:border-gray-700 dark:disabled:bg-gray-800/20"
-              type="name"
-              placeholder="Name"
-              required
-            />
-            <input
-              className="bg-white text-black w-56 h-10 px-5 rounded-lg duration-150 focus:invalid:text-pink-600 focus:border-sky-500 focus:outline focus:outline-sky-500 focus:invalid:border-pink-500 focus:invalid:outline-pink-500 disabled:border-gray-200 disabled:bg-gray-50 disabled:text-gray-500 disabled:shadow-none dark:disabled:border-gray-700 dark:disabled:bg-gray-800/20"
-              type="email"
-              placeholder="Email"
-              required
-            />
-            <div className="h-10">
-              <input
-                className="bg-white text-black w-56 h-10 px-5 pr-15 rounded-lg duration-150 focus:invalid:text-pink-600 focus:border-sky-500 focus:outline focus:outline-sky-500 focus:invalid:border-pink-500 focus:invalid:outline-pink-500 disabled:border-gray-200 disabled:bg-gray-50 disabled:text-gray-500 disabled:shadow-none dark:disabled:border-gray-700 dark:disabled:bg-gray-800/20"
-                type={showPassword ? "text" : "password"}
-                placeholder="Password"
-                required
-              />
-              <button
-                type="button"
-                className="absolute my-1 right-1 bg-gray-200 hover:bg-gray-300 rounded px-2 py-1"
-                onClick={togglePasswordVisibility}
-              >
-                {showPassword ? "Hide" : "Show"}
-              </button>
-            </div>
-            <button
-              className="bg-green-400 hover:bg-green-300 duration-150 text-black hover:font-bold w-56 h-10 rounded-lg flex justify-center items-center"
+              className="bg-green-400 hover:bg-green-300 cursor-pointer duration-150 text-black hover:font-bold w-56 h-10 rounded-lg flex justify-center items-center"
               type="submit"
             >
               Submit
