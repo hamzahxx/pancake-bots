884726d5cc23ff6b16cc0b75856ca175f4a7d9a8
Hamzah <hamzahchhapra2004@gmail.com>
2025-02-01 20:02:25 +0530

    animation/App.jsx
    - added animation to swaping of pages from authentication to company pages
    - divided the app into two sections of navbar and main content
    - reduced logic of adding username to the navbar when user is authenticated

--- src/App.jsx
+++ src/App.jsx
@@ -9,50 +9,79 @@ import Authentication from "./pages/Authentication";
 import Company from "./pages/Company";
 
 function App() {
-  // User State
+  // User data state
   const [userData, setUserData] = useState({});
-  const handleUserData = (data) => {
-    setUserData(data);
-  };
-  const { name, email, password, login } = userData;
+  const { name, email, login } = userData;
 
-  // Company State
+  const handleUserData = (data) => {
+    gsap.to("#component-container", {
+      opacity: 0,
+      duration: 0.3,
+      onComplete: () => {
+        setUserData(data);
+        gsap.to("#component-container", {
+          opacity: 1,
+          duration: 0.3,
+        });
+      },
+    });
+  };
+
+  // Company data state
   const [companyData, setCompanyData] = useState({});
   const handleCompanyData = (data) => {
     setCompanyData(data);
   };
 
-  // Animation Logic
   const container = useRef(null);
   useGSAP(
     () => {
       gsap.from("#title", {
-        delay: 0.2,
+        delay: 0.3,
         opacity: 0,
         duration: 0.5,
       });
     },
     { scope: container }
   );
+
+  useGSAP(
+    () => {
+      gsap.from(".nav-user", {
+        opacity: 0,
+        x: 20,
+        duration: 0.5,
+      });
+    },
+    { dependencies: [name, email], scope: container }
+  );
+
   return (
-    <div
-      ref={container}
-      className="bg-[#0B1120] flex flex-col h-[80vh] md:h-screen items-center justify-center"
-    >
-      <h1
-        id="title"
-        className="text-white absolute z-10 top-10 text-4xl font-bold"
-      >
-        BeyondChats
-      </h1>
-      <div className="relative z-10">
-        {!login && <Authentication onSubmit={handleUserData} />}
-        {login && <Company onSubmit={handleCompanyData} />}
+    <div className="bg-[#0b1120]" ref={container}>
+      <div className="relative z-10 flex justify-around items-center py-4">
+        <h1 id="title" className="text-white relative z-10 text-4xl font-bold">
+          BeyondChats
+        </h1>
+
+        {(name || email) && (
+          <h1 className="nav-user hidden md:block text-white relative z-10 text-3xl">
+            {name || email}
+          </h1>
+        )}
       </div>
-      <img
-        className="min-w-screen absolute bottom-0 lg:top-[-10px] z-[1]"
-        src={background}
-      />
+      <div className="flex flex-col h-[80vh] items-center justify-center">
+        <div className="relative z-10" id="component-container">
+          {!login ? (
+            <Authentication onSubmit={handleUserData} />
+          ) : (
+            <Company onSubmit={handleCompanyData} />
+          )}
+        </div>
+        <img
+          className="min-w-screen absolute bottom-0 lg:top-[-10px] z-[1]"
+          src={background}
+        />
+      </div>
     </div>
   );
 }
